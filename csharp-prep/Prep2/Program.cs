a81cb86515495c33023382ee64ff97dee1f57faa
Dallin Astling <djastling@users.noreply.github.com>
2024-04-27 15:44:22 -0600

    Prove Assignment 4/27/24

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int intGrade = int.Parse(grade);
+
+        string letter = "";
+        string plusMinus = "";
+        
+        if (intGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (intGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (intGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (intGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int remainder = intGrade % 10;
+
+        if (remainder >= 7)
+        {
+            plusMinus = "+";
+        }
+        else if (remainder < 3)
+        {
+            plusMinus = "-";
+        }
+
+        Console.WriteLine($"You got a {letter}{plusMinus}");
+        if (intGrade >= 70)
+        {
+            Console.WriteLine("You passed the class, congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass the class, better luck next time!");
+        }
+    }   
 }
